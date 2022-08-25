5eeca217270ac12214c0952bc71783090207bc92
Cyano Prem <cyano.prem@gmail.com>
2022-08-25 11:19:50 +0530

    Add comments

--- script.rb
+++ script.rb
@@ -1,7 +1,7 @@
 def caesar_cipher(string, shift)
-    string_to_ascii = string.bytes
-    shifted_integers = string_to_ascii.map do |num| 
-        if num >= 97 && num <= 122
+    string_to_ascii = string.bytes  #Convert the letters to ASCII Codes
+    shifted_integers = string_to_ascii.map do |num|     #Function to shift the ASCII Codes
+        if num >= 97 && num <= 122  #For smallcase letters
             if num + shift > 122
                 num = 96 + (num + shift - 122)
             elsif num + shift < 97
@@ -9,7 +9,7 @@ def caesar_cipher(string, shift)
             else
                 num = num + shift
             end
-        elsif num >= 65 && num <= 90
+        elsif num >= 65 && num <= 90 #For uppercase letters
             if num + shift > 90
                 num = 64 + (num + shift - 90)
             elsif num + shift < 65
@@ -17,11 +17,11 @@ def caesar_cipher(string, shift)
             else
                 num = num + shift
             end
-        else
+        else #For spaces and other characters
             num = num
         end
     end
-    p shifted_integers.map { |num| num.chr }.join 
+    p shifted_integers.map { |num| num.chr }.join #Shifting back to encrypted string
 end
 
 caesar_cipher("What a string!", 5)
